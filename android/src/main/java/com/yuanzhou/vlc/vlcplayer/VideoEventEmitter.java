ab8465c1f5d3a3c096cac8c7c5705083219db3c2
rich111 chen <rich111.chen@gmail.com>
2019-09-18 15:02:34 +0800

    migrate to AndroidX

--- android/src/main/java/com/yuanzhou/vlc/vlcplayer/VideoEventEmitter.java
+++ android/src/main/java/com/yuanzhou/vlc/vlcplayer/VideoEventEmitter.java
@@ -1,12 +1,11 @@
 package com.yuanzhou.vlc.vlcplayer;
 
-import android.support.annotation.StringDef;
-import android.util.Log;
+import androidx.annotation.StringDef;
+
 import android.view.View;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
 
